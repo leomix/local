cd7786719d8712b1b8e2680174fb499e1a2b0cae
LEONARDO ZAVALA <48141307+leomix@users.noreply.github.com>
2023-07-02 13:52:53 -0600

    Update buscador.php

--- buscador.php
+++ buscador.php
@@ -1,4 +1,5 @@
-<?php session_start();
+<?php 
+session_start();
 if( !isset( $_SESSION['token'] ) || time() - $_SESSION['login_time'] > 1800)
     header('Location: formulario.php');
 include 'functions.php';?>
